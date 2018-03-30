f7fa4cf64f2f33503c358ef3958b64c690381d3e
David Mascharka <davidmascharka@gmail.com>
2018-03-30 17:54:53 -0400

    Add identity initializer

---
+++ mynn/initializers/identity.py
@@ -0,0 +1,16 @@
+import numpy as np
+from mygrad import Tensor
+
+def identity(size):
+    ''' Initialize the identity matrix of dimension `size`x`size`.
+
+    Parameters
+    ----------
+    size : int
+        The size of the identity matrix.
+
+    Returns
+    mygrad.Tensor, shape=(`size`, `size`)
+        A Tensor, with ones on the main diagonal and zeros elsewhere.
+    '''
+    return Tensor(np.identity(size))
