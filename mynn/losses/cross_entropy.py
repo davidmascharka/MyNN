538a50c29a32f9d46013983a497d513c10d79c0a
David Mascharka <davidmascharka@gmail.com>
2018-03-31 17:44:13 -0400

    Add (weighted) cross-entropy loss

--- mynn/losses/cross_entropy.py
+++ mynn/losses/cross_entropy.py
@@ -3,8 +3,12 @@ from mygrad import Tensor
 import numpy as np
 
 class CrossEntropyLoss(Operation):
-    ''' Returns the cross-entropy loss between outputs and targets. '''
-    def __call__(self, outputs, targets):
+    ''' Returns the (weighted) cross-entropy loss between outputs and targets.
+
+    Note that this computes the softmax, and so should not take log-probabilities. If you
+    have log-probs, you should instead use :class:`NegativeLogLikelihood`.
+    '''
+    def __call__(self, outputs, targets, weights=None):
         '''
         Parameters
         ----------
@@ -13,12 +17,48 @@ class CrossEntropyLoss(Operation):
 
         targets : Sequence[int]
             The correct class indices, in [0, C), for each datum.
+
+        weights : Sequence[Real], optional (default=None)
+            The weighting factor to use on each class, or None.
        
         Returns
         -------
-        The average cross-entropy loss.
+        The average (weighted) cross-entropy loss.
         '''
-        raise NotImplementedError
+        self.variables = (outputs,)
+        scores = np.copy(outputs.data)
+        max_scores = np.max(scores, axis=1, keepdims=True)
+        np.exp(scores - max_scores, out=scores)
+        scores /= np.sum(scores, axis=1, keepdims=True)
+        label_locs = (range(len(scores)), targets)
+        factors = weights[targets] if weights is not None else np.ones_like(targets)
+        total_weight = np.sum(factors)
+        
+        loss = -np.sum(np.log(scores[label_locs]) * factors) / total_weight
 
-    def backward_a(self, grad):
-        raise NotImplementedError
+        self.back = scores
+        self.back[label_locs] -= 1
+        self.back *= factors.reshape(-1, 1) / total_weight
+        return loss
+
+    def backward_var(self, grad, index, **kwargs):
+        self.variables[index].backward(grad * self.back, **kwargs)
+
+def cross_entropy(x, y, weights=None):
+    '''
+    Parameters
+    ----------
+    x : mygrad.Tensor, shape=(N, C)
+        The C class scores for each of the N pieces of data.
+
+    y : Sequence[int]
+        The correct class indices, in [0, C), for each datum.
+
+    weights : Sequence[Real], optional (default=None)
+        The weighting factor to use on each class, or None.
+   
+    Returns
+    -------
+    The average cross-entropy loss.
+    '''
+    return Tensor._op(CrossEntropyLoss, x, op_args=(y, weights))
