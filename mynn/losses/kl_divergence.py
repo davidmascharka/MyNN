7282677eb73fb449b9964e61e7bb3c0f765229a9
David Mascharka <davidmascharka@gmail.com>
2018-03-31 20:33:37 -0400

    Add KL Divergence

--- mynn/losses/kl_divergence.py
+++ mynn/losses/kl_divergence.py
@@ -5,7 +5,7 @@ import numpy as np
 class KLDivergenceLoss(Operation):
     ''' Returns the Kullback-Leibler divergence loss from the outputs to the targets.
     
-    The KL-Divergence loss for a single sample is given by yᵢ*(log(yᵢ) - xᵢ)
+    The KL-Divergence loss for a single sample is given by yᵢ⊙(log(yᵢ) - xᵢ)
     '''
     def __call__(self, outputs, targets):
         '''
@@ -17,7 +17,26 @@ class KLDivergenceLoss(Operation):
         targets : numpy.ndarray, shape=(N, any)
             The correct vaue for each datum.
         '''
-        raise NotImplementedError
+        if isinstance(targets, Tensor):
+            targets = targets.data
 
-    def backward_a(self, grad):
-        raise NotImplementedError
+        self.variables = (outputs,)
+        loss = np.mean(targets * (np.log(targets) - outputs.data))
+
+        self.back = -np.maximum(targets, 0) / outputs.size
+        return loss
+
+    def backward_var(self, grad, index, **kwargs):
+        self.variables[index].backward(grad * self.back, **kwargs)
+
+def kl_divergence(x, y):
+    '''
+    Parameters
+    ----------
+    outputs : mygrad.Tensor, shape=(N, any)
+        The model outputs for each of the N pieces of data.
+
+    targets : numpy.ndarray, shape=(N, any)
+        The correct vaue for each datum.
+    '''
+    return Tensor._op(KLDivergenceLoss, x, op_args=(y,))
