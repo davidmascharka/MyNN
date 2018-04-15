b683e878d48030065102156afc83aa909878085b
David Mascharka <davidmascharka@gmail.com>
2018-04-15 10:33:42 -0400

    Update soft sign documentation, addressing #12

--- mynn/activations/soft_sign.py
+++ mynn/activations/soft_sign.py
@@ -1,5 +1,16 @@
 from mygrad import abs
 
 def soft_sign(x):
-    ''' Returns the soft sign function x/(1 + |x|) '''
+    ''' Returns the soft sign function x/(1 + |x|).
+
+    Parameters
+    ----------
+    x : mygrad.Tensor
+        Input data.
+
+    Returns
+    -------
+    numpy.ndarray
+        The soft sign function applied to `x` elementwise.
+    '''
     return x / (1 + abs(x))
