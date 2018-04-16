08f950544537e77e8645f732713a51eb579ba9d1
David Mascharka <davidmascharka@gmail.com>
2018-04-16 08:53:46 -0400

    Update leaky relu

--- mynn/activations/leaky_relu.py
+++ mynn/activations/leaky_relu.py
@@ -1,34 +1,6 @@
-import numpy as np
-from mygrad import Tensor
-from mygrad.operations import Operation
+from mygrad import maximum, minimum
 
 __all__ = ['leaky_relu']
-
-class LeakyReLU(Operation):
-    ''' Returns the leaky rectified linear activation elementwise along x. The leaky ReLU is given
-    by max(x, 0) + slope*min(x, 0)
-    '''
-    def __call__(self, x, slope):
-        '''
-        Parameters
-        ----------
-        x : mygrad.Tensor
-            Input data.
-
-        slope : Real
-            The slope of the negative activation.
-
-        Returns
-        -------
-        numpy.ndarray
-            The leaky-rectified `x` (elementwise max(x, 0) + slope*min(x, 0)).
-        '''
-        self.variables = (x,)
-        return np.maximum(x.data, 0) + slope*np.minimum(x.data, 0)
-
-    def backward_var(self, grad, index, **kwargs):
-        x = self.variables[index]
-        x.backward(grad, **kwargs)
 
 def leaky_relu(x, slope):
     ''' Returns the leaky rectified linear activation elementwise along x. The leaky ReLU is given
@@ -47,4 +19,4 @@ def leaky_relu(x, slope):
     numpy.ndarray
         The rectified `x` (elementwise max(x, 0)).
     '''
-    return Tensor._op(LeakyReLU, x, op_args=(slope,))
+    return maximum(x, 0) + slope * minimum(x, 0)
