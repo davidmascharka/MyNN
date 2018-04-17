fd137e3216545701f6e039302351ee837c071f98
David Mascharka <davidmascharka@gmail.com>
2018-04-17 09:49:39 -0400

    Add SGD

--- mynn/optimizers/sgd.py
+++ mynn/optimizers/sgd.py
@@ -8,11 +8,50 @@ class SGD:
     params : Iterable
         The parameters over which to optimize.
 
-    learning_rate : float, optional (default=0.1)
+    learning_rate : Real, optional (default=0.1)
         The step size.
 
-    momentum : float ∈ [0, 1), optional (default=0.0)
+    momentum : Real ∈ [0, 1), optional (default=0.0)
         The momentum term.
     '''
     def __init__(self, params, *, learning_rate=0.1, momentum=0.0):
-        raise NotImplementedError
+        assert 0 <= momentum < 1, 'Momentum must lie within [0, 1)'
+
+        self.params = params
+        self.learning_rate = learning_rate
+        self.momentum = momentum
+
+        self.param_moments = []
+        if momentum != 0:
+            for param in params:
+                self.param_moments.append(np.zeros_like(param.data))
+
+    def step(self):
+        ''' Perform one optimization step.
+
+        This function should be called after accumulating gradients into the parameters of the model
+        you wish to optimize via `backward()`. This will perform one step of (stochatic) gradient
+        descent, optionally with momentum. The update here is given by
+
+          ν = μν - η(dθ)
+          θ = θ + ν
+
+        where θ is the parameter list of the model, η is the learning rate, μ is the weighting on 
+        the momentum term, and ν is the moment of each parameter. Note that if the momentum term is
+        0, this simlifies to θ = θ - η(dθ)
+        '''
+        for idx, param in enumerate(self.params):
+            if param.grad is None:
+                continue
+
+            # perform the momentum update
+            if self.momentum != 0:
+                moment = self.param_moments[idx]
+                moment *= self.momentum
+                moment -= self.learning_rate * param.grad
+                update = moment
+            else:
+                update = -self.learning_rate * param.grad
+
+            # update parameters
+            param.data += update
