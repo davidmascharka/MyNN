9a6d3007c18d9f93b849c4aa145798951464c034
David Mascharka <davidmascharka@gmail.com>
2018-04-18 12:36:39 -0400

    Add softmax

---
+++ mynn/activations/softmax.py
@@ -0,0 +1,23 @@
+from mygrad import exp
+
+__all__ = ['softmax']
+
+def softmax(x):
+    ''' Returns the softmax exp(x) / Σexp(x).
+
+    Parameters
+    ----------
+    x : mygrad.Tensor
+        Input data.
+
+    Returns
+    -------
+    numpy.ndarray
+        The softmax of `x`.
+    '''
+    assert 0 < x.ndim < 3, 'Input must be 1- or 2-dimensional.'
+
+    kw = dict(axis=1, keepdims=True) if x.ndim == 2 else dict(axis=None, keepdims=False)
+    
+    x = exp(x - x.max(**kw))
+    return x / x.sum(**kw)
