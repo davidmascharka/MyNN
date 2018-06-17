86a0aa9a13bf097171a444643137eab9fd3dfb99
David Mascharka <davidmascharka@gmail.com>
2018-06-17 08:24:14 -0400

    Implement Adam

--- mynn/optimizers/adam.py
+++ mynn/optimizers/adam.py
@@ -28,5 +28,39 @@ class Adam:
       Diederik P. Kingma and Jimmy Ba
     https://arxiv.org/abs/1412.6980
     '''
-    def __init__(self, params, *, learning_rate=0.001, beta1=0.9, beta2=0.999, eps=1e-08):
-        raise NotImplementedError
+    def __init__(self, params, *, learning_rate=0.001, beta1=0.9, beta2=0.999, eps=1e-08,
+                 weight_decay=0):
+        self.params = params
+        self.learning_rate = learning_rate
+        self.beta1 = beta1
+        self.beta2 = beta2
+        self.eps = eps
+        self.t = 0  # timestep
+        self.m = [] # first moment estimate
+        self.v = [] # second moment estimate
+        for param in params:
+            self.m.append(np.zeros_like(param.data))
+            self.v.append(np.zeros_like(param.data))
+        self.weight_decay = weight_decay
+        
+    def step(self):
+        ''' Perform one optimization step.
+
+        This function should be called after accumulating gradients into the parameters of the model
+        you wish to optimize via `backward()`. This will perform one step of the Adam optimization
+        algorithm put forward by Kingma and Ba.
+        '''
+        self.t += 1
+        for idx, param in enumerate(self.params):
+            if param.grad is None:
+                continue
+
+            self.m[idx] = self.beta1*self.m[idx] + (1 - self.beta1)*param.grad
+            self.v[idx] = self.beta2*self.v[idx] + (1 - self.beta2)*param.grad**2
+
+            m_hat = self.m[idx] / (1 - self.beta1**self.t)
+            v_hat = self.v[idx] / (1 - self.beta2**self.t)
+
+            update = -self.weight_decay * param.data if param.ndim > 1 else 0
+            update += -self.learning_rate * m_hat / (np.sqrt(v_hat) + self.eps)
+            param.data += update
