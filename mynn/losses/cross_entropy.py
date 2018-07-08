106160d45ef6176d03702310f27783ba40fe5da4
David Mascharka <davidmascharka@gmail.com>
2018-07-08 11:26:50 -0400

    Update cross-entropy loss

--- mynn/losses/cross_entropy.py
+++ mynn/losses/cross_entropy.py
@@ -1,55 +1,7 @@
-from mygrad.operation_base import Operation
-from mygrad import Tensor
-import numpy as np
+from mynn.activations.log_softmax import log_softmax
+from mynn.losses.negative_log_likelihood import negative_log_likelihood
 
-class CrossEntropyLoss(Operation):
-    ''' Returns the (weighted) cross-entropy loss between outputs and targets.
-
-    Note that this computes the softmax, and so should not take log-probabilities. If you
-    have log-probs, you should instead use :class:`NegativeLogLikelihood`.
-    '''
-    def __call__(self, outputs, targets, weights=None):
-        '''
-        Parameters
-        ----------
-        outputs : mygrad.Tensor, shape=(N, C)
-            The C class scores for each of the N pieces of data.
-
-        targets : Sequence[int]
-            The correct class indices, in [0, C), for each datum.
-
-        weights : Sequence[Real], optional (default=None)
-            The weighting factor to use on each class, or None.
-       
-        Returns
-        -------
-        The average (weighted) cross-entropy loss.
-        '''
-        if isinstance(targets, Tensor):
-            targets = targets.data
-
-        if isinstance(weights, Tensor):
-            weights = weights.data
-
-        self.variables = (outputs,)
-        scores = np.copy(outputs.data)
-        max_scores = np.max(scores, axis=1, keepdims=True)
-        np.exp(scores - max_scores, out=scores)
-        scores /= np.sum(scores, axis=1, keepdims=True)
-        label_locs = (range(len(scores)), targets)
-        factors = weights[targets] if weights is not None else np.ones_like(targets)
-        total_weight = np.sum(factors)
-        
-        loss = -np.sum(np.log(scores[label_locs] + 1e-14) * factors) / total_weight
-        self.back = scores
-        self.back[label_locs] -= 1
-        self.back *= factors.reshape(-1, 1) / total_weight
-        return loss
-
-    def backward_var(self, grad, index, **kwargs):
-        self.variables[index].backward(grad * self.back, **kwargs)
-
-def cross_entropy(x, y, *, weights=None):
+def softmax_cross_entropy(x, y, *, weights=None):
     '''
     Parameters
     ----------
@@ -66,4 +18,4 @@ def cross_entropy(x, y, *, weights=None):
     -------
     The average cross-entropy loss.
     '''
-    return Tensor._op(CrossEntropyLoss, x, op_args=(y, weights))
+    return negative_log_likelihood(log_softmax(x), y)
