7b87812ba358902ab2a98dfbe7a4933ceffc3e33
Ryan Soklaski <ry26099@mit.edu>
2018-07-13 11:20:50 -0400

    improve docs

--- mynn/activations/log_softmax.py
+++ mynn/activations/log_softmax.py
@@ -3,20 +3,21 @@ from mygrad.nnet.activations import logsoftmax as mygrad_log_softmax
 __all__ = ['log_softmax']
 
 def log_softmax(x):
-    ''' Returns the log softmax:
-          log(exp(x) / Σexp(x)).
+    ''' Returns the log softmax of x along its trailing dimension:
+
+        log(exp(x) / Σexp(x))
 
     However, this implementation is faster and more numerically-stable
     than performing the softmax followed by the log.
 
     Parameters
     ----------
-    x : mygrad.Tensor
+    x : mygrad.Tensor, shape=(N, C) or shape=(C,)
         Input data.
 
     Returns
     -------
-    mygrad.Tensor
+    mygrad.Tensor, shape=(N,) or shape=()
         The log-softmax of `x`.
     '''
     return mygrad_log_softmax(x)
