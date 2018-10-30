f8d7e51c6e7effaa6184590def90ba4ed430ef1e
David Mascharka <davidmascharka@users.noreply.github.com>
2018-10-30 12:26:12 -0500

    Fix grammar (#39)

--- mynn/activations/hard_tanh.py
+++ mynn/activations/hard_tanh.py
@@ -3,7 +3,7 @@ from mygrad import maximum, minimum
 __all__ = ['hard_tanh']
 
 def hard_tanh(x, *, lower_bound=-1, upper_bound=1):
-    """ Returns the hard hypterbolic tangent function.
+    """ Returns the hard hyperbolic tangent function.
 
     The hard_tanh function is `lower_bound` where `x` <= `lower_bound`, `upper_bound` where 
     `x` >= `upper_bound`, and `x` where `lower_bound` < `x` < `upper_bound`.
