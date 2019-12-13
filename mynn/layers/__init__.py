c728d9168d9fd37645f434d8a98eb542515a3405
David Mascharka <davidmascharka@users.noreply.github.com>
2019-12-13 09:48:06 -0800

    Make sane inits (#51)

---
+++ mynn/layers/__init__.py
@@ -0,0 +1,7 @@
+from .batchnorm import batchnorm
+from .conv import conv
+from .dense import dense
+from .dropout import dropout
+from .prelu import prelu
+
+__all__ = (batchnorm, conv, dense, dropout, prelu)
---
+++ mynn/optimizers/__init__.py
@@ -0,0 +1,8 @@
+from .adadelta import Adadelta
+from .adagrad import AdaGrad
+from .adamax import AdaMax
+from .adam import Adam
+from .rmsprop import RMSprop
+from .sgd import SGD
+
+__all__ = (Adadelta, AdaGrad, AdaMax, Adam, RMSprop, SGD)
