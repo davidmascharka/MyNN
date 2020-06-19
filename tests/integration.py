d7e1369a3a14a0b7e3939cd3c5a5989f3443c8bf
David Mascharka <davidmascharka@gmail.com>
2020-06-19 13:48:06 -0700

    Simplify

--- tests/integration.py
+++ tests/integration.py
@@ -3,7 +3,10 @@ Train the examples and ensure performance is acceptable. This is intended to be 
 nothing in this package breaks when MyGrad updates until they're merged.
 """
 
+import gzip
+import os
 from pathlib import Path
+import urllib.request
 
 import numpy as np
 
@@ -15,20 +18,10 @@ from mynn.optimizers import SGD
 from mygrad.nnet.layers.pooling import max_pool
 
 
-def _md5_check(fname):
-    """ Reads in data from disk and returns md5 hash"""
-    import hashlib
-    hash_md5 = hashlib.md5()
-    with open(fname, "rb") as f:
-        for chunk in iter(lambda: f.read(4096), b""):
-            hash_md5.update(chunk)
-    return hash_md5.hexdigest()
+def download_mnist(path=Path("mnist.npz"), server_url="http://yann.lecun.com/exdb/mnist/", tmp_file="__mnist.bin"):
+    if Path(path).is_file():
+        return None  # already exists
 
-
-def _download_mnist(path, server_url, tmp_file, check_sums=None):
-    import urllib.request
-    import gzip
-    import os
     urls = dict(
         tr_img="train-images-idx3-ubyte.gz",
         tr_lbl="train-labels-idx1-ubyte.gz",
@@ -46,16 +39,6 @@ def _download_mnist(path, server_url, tmp_file, check_sums=None):
                 with open(tmp_file, "wb") as handle:
                     handle.write(response.read())
 
-                if check_sums is not None and isinstance(check_sums[urls[img_key]], str):
-                    # check md5
-                    expected = check_sums[urls[img_key]]
-                    found = _md5_check(tmp_file)
-                    msg = f"md5 checksum did not match!.. deleting file:\nexpected: {expected}\nfound: {found}"
-                    assert expected == found, msg
-                elif check_sums is not None and isinstance(check_sums[urls[img_key]], int):
-                    msg = "downloaded filesize is bad!.. deleting file"
-                    assert os.path.getsize(tmp_file) == check_sums[urls[img_key]], msg
-
                 with gzip.open(tmp_file, "rb") as uncompressed:
                     tmp = np.frombuffer(uncompressed.read(), dtype=np.uint8, offset=16)
             finally:
@@ -67,16 +50,6 @@ def _download_mnist(path, server_url, tmp_file, check_sums=None):
             try:
                 with open(tmp_file, "wb") as handle:
                     handle.write(response.read())
-
-                if check_sums is not None and isinstance(check_sums[urls[img_key]], str):
-                    # check md5
-                    expected = check_sums[urls[lbl_key]]
-                    found = _md5_check(tmp_file)
-                    msg = f"md5 checksum did not match!.. deleting file:\nexpected: {expected}\nfound: {found}"
-                    assert expected == found, msg
-                elif check_sums is not None and isinstance(check_sums[urls[img_key]], int):
-                    msg = "downloaded filesize is bad!.. deleting file"
-                    os.path.getsize(tmp_file) == check_sums[urls[img_key]], msg
 
                 with gzip.open(tmp_file, "rb") as uncompressed:
                     tmp_lbls = np.frombuffer(uncompressed.read(), dtype=np.uint8, offset=8)
@@ -94,22 +67,6 @@ def _download_mnist(path, server_url, tmp_file, check_sums=None):
         )
 
 
-def download_mnist():
-    path = "mnist.npz"
-    if Path(path).is_file():
-        return None  # already exists
-
-    server_url = "http://yann.lecun.com/exdb/mnist/"
-
-    check_file_sizes = {
-        "train-images-idx3-ubyte.gz": 9912422,
-        "train-labels-idx1-ubyte.gz": 28881,
-        "t10k-images-idx3-ubyte.gz": 28881,
-        "t10k-labels-idx1-ubyte.gz": 4542,
-    }
-    _download_mnist(path, server_url=server_url, tmp_file="__mnist.bin", check_sums=check_file_sizes)
-
-
 def load_mnist(fname="mnist.npz"):
     with np.load(fname) as data:
         out = tuple(data[str(key)] for key in ["x_train", "y_train", "x_test", "y_test"])
@@ -118,44 +75,27 @@ def load_mnist(fname="mnist.npz"):
 
 
 class ToyData:
-    def __init__(self, num_classes=3, points_per_class=120, num_revolutions=1.0, tendril_noise=0.2):
-        """
-        Parameters
-        ----------
-        num_classes : int, optional (default=3)
-            Number of tendrils.
-        points_per_class : int, optional (default=120)
-            Number of points generated in each tendril.
-        num_revolutions : float, optional (default=1.)
-            The number of full rotations to be completed by each tendril.
-        tendril_noise : float, optional (default=0.2)
-            Sets the scale of the noise used to scatter the points associated with each
-            tendril. A value of 0 produces pristine tendrils.
-        """
-        N = points_per_class  # number of points per class
-        D = 2  # dimensionality
-        K = num_classes  # number of classes
+    def __init__(self):
+        n_train = round(1000 // 1.2)
+        n_val = 1000 - n_train
+        self._coords = np.zeros((1000 * 3, 2))
+        self._labels = np.zeros(1000 * 3, dtype=np.uint8)
 
-        n_train = round(N // 1.2)
-        n_val = N - n_train
-        self._coords = np.zeros((N * K, D))  # coordinates
-        self._labels = np.zeros(N * K, dtype="uint8")  # class labels (used for plotting)
-
-        y_labels = np.zeros((N * K, K), dtype="uint8")
-        for j in range(K):
-            ix = range(N * j, N * (j + 1))
-            r = np.linspace(0.0, 1, N)  # radius
-            t = np.linspace(0, 2 * np.pi * num_revolutions, N) + np.random.randn(N) * tendril_noise  # theta
-            t += j / K * 2 * np.pi  # phase-shift to offset subsequent tendrils
+        y_labels = np.zeros((1000 * 3, 3), dtype=np.uint8)
+        for j in range(3):
+            ix = range(1000 * j, 1000 * (j + 1))
+            r = np.linspace(0.0, 1, 1000)
+            t = np.linspace(0, 2 * np.pi, 1000) + np.random.randn(1000) * 0.2
+            t += j / 3 * 2 * np.pi
             self._coords[ix] = np.column_stack((r * np.sin(t), r * np.cos(t)))
             self._labels[ix] = j
             y_labels[ix, j] = 1
 
         train_ids = np.concatenate(
-            [np.random.choice(range(N * i, N * (i + 1)), n_train, replace=False) for i in range(K)]
+            [np.random.choice(range(1000 * i, 1000 * (i + 1)), n_train, replace=False) for i in range(K)]
         )
-        train_ids = np.random.choice(train_ids, K * n_train, replace=False)
-        y_ids = np.random.choice(list(set(range(K * N)) - set(train_ids)), K * n_val, replace=False)
+        train_ids = np.random.choice(train_ids, 3 * n_train, replace=False)
+        y_ids = np.random.choice(list(set(range(3 * 1000)) - set(train_ids)), 3 * n_val, replace=False)
 
         self.x_train = self._coords[train_ids,].astype("float32")
         self.y_train = y_labels[train_ids,].astype("float32")
@@ -168,9 +108,9 @@ class ToyData:
 
 
 class SpiralModel:
-    def __init__(self, *, num_neurons=100, num_classes=3, num_input_dims=2):
-        self.layer1 = dense(num_input_dims, num_neurons)
-        self.layer2 = dense(num_neurons, num_classes)
+    def __init__(self):
+        self.layer1 = dense(2, 100)
+        self.layer2 = dense(100, 3)
 
     def __call__(self, x):
         return self.layer2(relu(self.layer1(x)))
@@ -213,11 +153,11 @@ def eval_epoch(test_data, test_labels, model, batch_size=64):
 
 
 def train_spiral():
-    toy_data = ToyData(points_per_class=1000, num_classes=3)
+    toy_data = ToyData()
 
     train_data, train_labels, test_data, test_labels = toy_data.load_data()
 
-    model = SpiralModel(num_neurons=100, num_classes=3, num_input_dims=2)
+    model = SpiralModel()
     optim = SGD(model.parameters, learning_rate=0.01, momentum=0.99)
     for _ in range(30):
         train_epoch(train_data=train_data, train_labels=train_labels, model=model, optim=optim)
