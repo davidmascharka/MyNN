ff281b083d461a1c247cc1d42f060002b71f5739
Ryan Soklaski <ry26099@mit.edu>
2021-02-06 12:01:34 -0500

    defer to mygrad for initializers

--- mynn/initializers/identity.py
+++ mynn/initializers/identity.py
@@ -1,17 +1,1 @@
-import numpy as np
-from mygrad import Tensor
-
-
-def identity(size):
-    """ Initialize the identity matrix of dimension `size`x`size`.
-
-    Parameters
-    ----------
-    size : int
-        The size of the identity matrix.
-
-    Returns
-    mygrad.Tensor, shape=(`size`, `size`)
-        A Tensor, with ones on the main diagonal and zeros elsewhere.
-    """
-    return Tensor(np.identity(size))
+from mygrad.nnet.initializers import identity
