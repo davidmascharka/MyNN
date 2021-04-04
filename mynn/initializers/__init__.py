16b558b6777a2c80481ef8fbfc23de91d8e5f112
David Mascharka <davidmascharka@users.noreply.github.com>
2021-04-04 15:27:12 -0400

    Use mygrad initializers (#59)

--- mynn/initializers/__init__.py
+++ mynn/initializers/__init__.py
@@ -1,14 +1,4 @@
-from .constant import constant
-from .dirac import dirac
-from .glorot_normal import glorot_normal
-from .glorot_uniform import glorot_uniform
-from .he_normal import he_normal
-from .he_uniform import he_uniform
-from .identity import identity
-from .normal import normal
-from .uniform import uniform
-
-__all__ = (
+from mygrad.nnet.initializers import (
     constant,
     dirac,
     glorot_normal,
@@ -18,4 +8,16 @@ __all__ = (
     identity,
     normal,
     uniform,
+)
+
+__all__ = (
+    "constant",
+    "dirac",
+    "glorot_normal",
+    "glorot_uniform",
+    "he_normal",
+    "he_uniform",
+    "identity",
+    "normal",
+    "uniform",
 )
